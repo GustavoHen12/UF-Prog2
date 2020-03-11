570195a97feb88ddf7f69000eb5444435076eff7
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-03-11 10:25:29 -0300

    começo exercicio

--- codCaracter/ex1.c
+++ codCaracter/ex1.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Escreva um programa em C para informar o número de caracteres presentes em um arquivo de texto.*/
 int main(int argc, char *argv[ ])
 {
-	FILE* arq;
-	arq = fopen(argv[1], "r");
-	char letra;
-	long int quant = 0;
-	while(!feof(arq))
+	unsigned char letra;
+	char conv[256];
+	int i = 0;
+	for(i = 192; i <= 197; i++) conv[i] = 'A';
+	for(i = 200; i <= 203; i++) conv[i] = 'E';
+	for(i = 204; i <= 207; i++) conv[i] = 'I';
+	for(i = 210; i <= 214; i++) conv[i] = 'O';
+	for(i = 217; i <= 220; i++) conv[i] = 'U';
+	for(i = 224; i <= 229; i++) conv[i] = 'a';
+	for(i = 232; i <= 235; i++) conv[i] = 'e';
+	for(i = 236; i <= 239; i++) conv[i] = 'i';
+	for(i = 242; i <= 246; i++) conv[i] = 'o';
+	for(i = 249; i <= 252; i++) conv[i] = 'u';
+	conv[199] = 'C';
+	conv[231] = 'c';
+	while(!feof(stdin))
 	{
-		letra = getc(arq);
-		if(letra > 33)
-			quant++;
+		letra = getchar();
+		if(letra > 160)
+			letra = conv[letra];
+		printf("%c", letra);
 	}
-	printf("> %ld\n", quant);
 	return 0;
 }
