c3d2535369f57ee67201084d81de05827069cebe
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-03-12 10:27:04 -0300

    inicio exercicios

---
+++ codCaracter/ex3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[ ])
+{
+	unsigned char letra;
+	char conv[256];
+	int i = 0;
+	for(i = 192; i <= 197; i++) conv[i] = 'A';
+	for(i = 200; i <= 203; i++) conv[i] = 'E';
+	for(i = 204; i <= 207; i++) conv[i] = 'I';
+	for(i = 210; i <= 214; i++) conv[i] = 'O';
+	for(i = 217; i <= 220; i++) conv[i] = 'U';
+	for(i = 224; i <= 229; i++) conv[i] = 'a';
+	for(i = 232; i <= 235; i++) conv[i] = 'e';
+	for(i = 236; i <= 239; i++) conv[i] = 'i';
+	for(i = 242; i <= 246; i++) conv[i] = 'o';
+	for(i = 249; i <= 252; i++) conv[i] = 'u';
+	conv[199] = 'C';
+	conv[231] = 'c';
+	while(!feof(stdin))
+	{
+		letra = getchar();
+		if(letra > 160)
+			letra = conv[letra];
+		printf("%c", letra);
+	}
+	return 0;
+}
